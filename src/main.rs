11e20c1beb6fbe9293bf75f5b9703c932906cf41
尼亚 <hello@nyah.dev>
2021-02-11 18:19:21 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+//! `XX` contains files for the crate.
+//!
+//! Then run `cargo run` in the toplevel directory of any crate or workspace.
+//!
+//! If you wish to consume its core functionality as a library, see the
+//! documentation for the `XX` crate.
+fn main() {
+    println!("Hello, world!");
+}
