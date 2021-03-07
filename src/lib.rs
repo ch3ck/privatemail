8ccf3e5abcc2fef308630beca399398087725391
Nyah Check <hello@nyah.dev>
2021-03-06 23:15:30 -0800

    Update lib package and added formatting fixes to config sub module

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
 #![recursion_limit = "256"]
 #![allow(clippy::field_reassign_with_default)]
 
-pub mod configs;
+pub mod config;
 
 #[macro_use]
 extern crate diesel;
 
 #[macro_use]
-extern crate serde_json
-extern crate dropshot
+extern crate serde_json;
+extern crate dropshot;
+
+/** Test module for privatemail package */
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
