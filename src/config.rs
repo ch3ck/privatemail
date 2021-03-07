436271c412ddc7ad5f885d558020b608b0824748
Nyah Check <hello@nyah.dev>
2021-03-06 23:15:30 -0800

    Update lib package and added formatting fixes to config sub module

--- src/config.rs
+++ src/config.rs
@@ -1,38 +1,38 @@
 //! Copyright 2021 Nyah Check crate.
-//! 
+//!
 //! Application-specific configuration for PrivatEmail
-use std::collections::HashMap;
 use serde::Deserialize;
 use serde::Serialize;
+use std::collections::HashMap;
 
-//!
-//! Configuration for a PrivatEmail server for SES.
-//!
-//! This type implements [`serde::Deserialize`] and [`serde::Serialize`] and it
-//! can be composed with the consumer's configuration (whatever format that's
-//! in).  For example, consumers could define a custom `AppConfig` for an app
-//! that contains uses PrivatEmail server:
-//!
-//! ```
-//! use privatemail::EmailConfig;
-//! use serde::Deserialize;
-//!
-//! #[derive(Deserialize)]
-//! struct AppConfig {
-//!     email_server: EmailConfig,
-//!     /* other app related configs */
-//! }
-//!
-//! fn main() -> Result<(), String> {
-//!     let my_config: EmailConfig = toml::from_str(
-//!         r##"
-//!             [email_server]
-//!
-//!             ## ... (other app-specific config)
-//!         "##
-//!     ).map_err(|error| format!("parsing server config: {}", error))?;
-//! ```
-//!
+/**
+ * Configuration for a PrivatEmail server for SES.
+ *
+ * This type implements [`serde::Deserialize`] and [`serde::Serialize`] and it
+ * can be composed with the consumer's configuration (whatever format that's
+ * in).  For example, consumers could define a custom `AppConfig` for an app
+ * that contains uses PrivatEmail server:
+ *
+ * ```
+ * use privatemail::EmailConfig;
+ * use serde::Deserialize;
+ *
+ * #[derive(Deserialize)]
+ * struct AppConfig {
+ *     email_server: EmailConfig,
+ *     /* other app related configs */
+ * }
+ *
+ * fn main() -> Result<(), String> {
+ *     let my_config: EmailConfig = toml::from_str(
+ *         r##"
+ *             [email_server]
+ *
+ *             ## ... (other app-specific config)
+ *         "##
+ *     ).map_err(|error| format!("parsing server config: {}", error))?;
+ * ```
+ */
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 #[serde(default)]
 pub struct EmailConfig {
@@ -47,13 +47,13 @@ pub struct EmailConfig {
     /**  Enables support for plus(+) sign suffixes on email addresses */
     pub allow_plus: bool,
     /** email_mapping: Dictionary mapping showing where to forward the emails */
-    pub email_mapping: HashMap<&str, &str>
+    pub email_mapping: HashMap<&str, &str>,
 }
 
 impl Default for EmailConfig {
     fn default() -> Self {
         let mut email_map: HashMap<&str, &str> = HashMap::with_capacity(1);
-        email_map.insert("@nyah.dev", "nyah@hey.com")
+        email_map.insert("@nyah.dev", "nyah@hey.com");
 
         EmailConfig {
             from_email: String::from("hello@nyah.dev"),
