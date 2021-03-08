35e3136eded82b62820f704cc96de7280ca31d5b
Nyah Check <hello@nyah.dev>
2021-03-08 00:22:51 -0800

    Add lambda function creation and sns topic subscriptions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,31 @@
-variable "bucket_name" {
+variable "bucket" {
   description = "Value of the S3 bucket name"
   type        = string
   default     = "nyah-ses-emails"
+}
+
+variable "function_name" {
+  default     = "ses-forward-emails-lambda-nyah"
+  description = "Value of Lambda function name"
+  type        = string
+}
+
+variable "topic" {
+  default     = "ses-email-forward-sns-topic-nyah"
+  type        = string
+  description = "SES email forwarding sns topic"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "organization" {
+  default     = "nyahdev"
+  description = "Terraform cloud organization name"
+}
+
+variable "workspace" {
+  default     = "nyah-dot-dev-workspace"
+  description = "Terraform cloud workspace name"
 }
