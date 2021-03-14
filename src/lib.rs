9807743f02a91051667cd7e55e2baa09b4d03ebc
Nyah Check <hello@nyah.dev>
2021-03-14 14:43:55 -0700

    Add updates to lib module and config

--- src/lib.rs
+++ src/lib.rs
@@ -20,10 +20,13 @@
 //! }
 //! ```
 #![allow(clippy::field_reassign_with_default)]
+mod config;
 use serde::de::DeserializeOwned;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use serde_json;
 use std::collections::HashMap;
+use std::fmt;
+use std::fmt::Debug;
 
 /** Test module for privatemail package */
 #[cfg(test)]
@@ -37,7 +40,8 @@ mod tests {
 // LambdaRequest: Represents the incoming Request from AWS Lambda
 //                This is deserialized into a struct payload
 //
-#[derive(Serialize, Debug)]
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(default)]
 pub struct LambdaRequest<Data: DeserializeOwned> {
     #[serde(deserialize_with = "deserialize")]
     /** lambda request body */
@@ -51,19 +55,19 @@ impl<Data: DeserializeOwned> LambdaRequest<Data> {
 }
 
 /// LambdaResponse: The Outgoing response being passed by the Lambda
-#[derive(Serialize, Debug)]
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct LambdaResponse {
-    #[serde(rename = "isBase64Encoded")]
     /** is_base_64_encoded response field */
     is_base_64_encoded: bool,
 
-    #[serde(rename = "statusCode")]
     /** status_code for lambda response */
     status_code: u32,
 
     /** response headers for lambda response */
     headers: HashMap<String, String>,
     /** response body for LambdaResponse struct */
+    #[serde(default, skip_serializing_if = "String::is_empty")]
     body: String,
 }
 
@@ -81,5 +85,16 @@ impl LambdaResponse {
             headers: header,
             body: serde_json::to_string(&body).unwrap(),
         }
+    }
+}
+
+impl fmt::Display for LambdaResponse {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "LambdaResponse: status_code: {}, body: {}",
+            self.status_code,
+            self.body.to_string()
+        )
     }
 }
