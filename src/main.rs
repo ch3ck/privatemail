d46d6872c16eb8d6de5fb8d16bebafc05828b43f
Nyah Check <hello@nyah.dev>
2021-03-15 00:28:55 -0700

    Update email config docs

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,6 @@
 //! This service is for forwarding reaw emails from S3 to SES.
 //! Configures S3 bucket with required prefixes to store raw emails and mapping for email addresses
 //! needed to forwared the emails.
-//!
-//! EmailConfig:
-//! from_email: Forwarded emails will be received from this SES verified email address.
-//!             To match all email addresses on a domain, use a key without the name part of the email(`example.com`)
-//! to_email: Recipient email address
-//!             Example: jon@doe.example
-//! subject_prefix: Forwarded emails subject will contain this prefix.
-//! email_bucket: S3 bucket to store raw SES emails.
-//! email_key_prefix: S3 key prefix where SES stores emails.
 #![allow(clippy::style)]
 mod lib;
 use lib::PrivatEmail_Handler;
