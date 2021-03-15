dc342822b5778ba901ebc9bd622ba2001ef4a420
Nyah Check <hello@nyah.dev>
2021-03-15 00:09:59 -0700

    Update cargo toml and main rs

--- src/main.rs
+++ src/main.rs
@@ -7,60 +7,21 @@
 //!
 //! EmailConfig:
 //! from_email: Forwarded emails will be received from this SES verified email address.
+//!             To match all email addresses on a domain, use a key without the name part of the email(`example.com`)
+//! to_email: Recipient email address
+//!             Example: jon@doe.example
 //! subject_prefix: Forwarded emails subject will contain this prefix.
 //! email_bucket: S3 bucket to store raw SES emails.
 //! email_key_prefix: S3 key prefix where SES stores emails.
-//! allow_plus: Enables support for plus(+) sign suffixes on email addresses.
-//!     Once this is set, the username is parsed to remove everything after the `+` sign
-//!     Example: (
-//!         jon+doe@doe.example,
-//!         jon+test@doe.example
-//!     ) - > will all be treated as => (
-//!         jon@doe.example
-//!     )
-//! email_mapping: Dictionary mapping showing where to forward the emails
-//!                 "sender": ["recipient", "emails"]
-//!                To match all email addresses on a domain, use a key without the name part of an email address( `@example.com`)
-//!                To match a mailbox on all domains, use a key without the `@` symbol e.g (`info`, 'admin')
-use lambda_runtime::{handler_fn, Context, Error};
-use tracing::log::LevelFilter;
-use serde::{Deserialize, Serialize};
-use simple_logger::SimpleLogger;
+#![allow(clippy::style)]
+mod lib;
+use lib::PrivatEmail_Handler;
+use lambda_runtime::{handler_fn, Error};
 
-
-#[derive(Deserialize)]
-struct Request {
-    command: String,
-}
-
-
-#[derive(Serialize)]
-struct Response {
-    req_id: String,
-    msg: String,
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // required to enable CloudWatch error logging by the runtime
-    // can be replaced with any other method of initializing `log`
-    SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
-
-    let func = handler_fn(my_handler);
-    lambda_runtime::run(func).await?;
+    let func = handler_fn(PrivatEmail_Handler);
+    lambda_runtime::run(PrivatEmail_Handler).await?;
     Ok(())
-}
-
-pub(crate) async fn my_handler(event: Request, ctx: Context) -> Result<Response, Error> {
-    // extract some useful info from the request
-    let command = event.command;
-
-    // prepare the response
-    let resp = Response {
-        req_id: ctx.request_id,
-        msg: format!("Command {} executed.", command),
-    };
-
-    // return `Response` (it will be serialized to JSON automatically by the runtime)
-    Ok(resp)
 }
