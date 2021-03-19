f9cede84aa92c2a8fdcf19b9c0de9b1a2ed5f866
Nyah Check <hello@nyah.dev>
2021-03-18 22:47:53 -0700

    Skip integration tests on condition

--- src/lib.rs
+++ src/lib.rs
@@ -214,10 +214,10 @@ mod tests {
     }
 
     #[tokio::test]
-    // #[ignore]
+    #[ignore = "skipping integration because because of IAM requirements"]
     async fn handler_handles() {
-        env::set_var("TO_EMAIL", "nyah@hey.com");
-        env::set_var("FROM_EMAIL", "test@nyah.dev");
+        env::set_var("TO_EMAIL", "test@nyah.dev");
+        env::set_var("FROM_EMAIL", "achu@fufu.africa");
         let test_event = read_test_event();
         assert_eq!(
             privatemail_handler(test_event, Context::default())
