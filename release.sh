105e5f42b54a48c04a2c3d0b6a756bd52798048e
Nyah Check <4006891+ch3ck@users.noreply.github.com>
2022-06-25 00:48:52 -0700

    Add 0.2.2 release

--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ rustup target add x86_64-unknown-linux-musl
 docker pull clux/muslrust
 docker run -v $PWD:/volume --rm -t clux/muslrust cargo build --release --target x86_64-unknown-linux-musl
 zip -j lambda.zip ./target/x86_64-unknown-linux-musl/release/bootstrap
-ln -s lambda.zip terraform/lambda.zip
+cp lambda.zip terraform/lambda.zip
 
 echo "Terraform Provisioning"
 terraform -chdir=terraform init
@@ -16,5 +16,6 @@ terraform validate -json
 terraform -chdir=terraform plan
 terraform -chdir=terraform apply -auto-approve
 
+rm terraform/lambda.zip lambda.zip
 echo ">>>>>Release complete<<<<"
 set +eux
